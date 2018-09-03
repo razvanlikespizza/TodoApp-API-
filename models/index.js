f77869f05b2d49617c487802ddec425eccc9c409
razvanlikespizza <razvanro666@gmail.com>
2018-09-03 13:21:33 +0300

    back

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 
 mongoose.set("debug", true);
-mongoose.connect("mongodb://razvan:razvan123321@ds251807.mlab.com:51807/razvan_yelpcamp");
+mongoose.connect("mongodb://localhost/todoapi");
 mongoose.Promise = Promise; // use promises instead of callbacks
 
 module.exports.Todo = require("./todo");
