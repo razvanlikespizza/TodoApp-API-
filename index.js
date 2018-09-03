1ba12abd3cb33518e23e7933587d5645e0849312
Razvan <razvanro666@gmail.com>
2018-09-03 12:09:25 +0300

    Test

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    todoRoutes = require("./routes/todos");
+    todoRoutes = require("./routes/todos"),
+    https = require ("https");
+const PROD = false;
+const lex = require('greenlock-express').create({
+  version: 'draft-11',
+  server: PROD ? 'https://acme-v02.api.letsencrypt.org/directory' : 'https://acme-staging-v02.api.letsencrypt.org/directory',
+  approveDomains: (opts, certs, cb) => {
+    if (certs) {
+      // change domain list here
+      opts.domains = ['raz.h4ck.me', '176.126.189.55']
+    } else { 
+      // change default email to accept agreement
+      opts.email = 'razvanro666@gmail.com'; 
+      opts.agreeTos = true;
+    }
+    cb(null, { options: opts, certs: certs });
+  }
+  // optional: see "Note 3" at the end of the page
+  // communityMember: true
+});
+const middlewareWrapper = lex.middleware;
 
 app.use(bodyParser.json()); //adding body parser to acces req.body
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,7 +34,10 @@ app.get("/", function(req, res) {
   res.sendFile("index.html");
 });
 
-
-app.listen(8080, function() {
+https.createServer(
+  lex.httpsOptions, 
+  middlewareWrapper(handler)
+).listen(8080);
+/*app.listen(8080, function() {
   console.log("==========START============");
-});
+});*/
